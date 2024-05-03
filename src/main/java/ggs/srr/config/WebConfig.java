83e7495b707866fea75423d026421c512f3b197e
joonwan <joonwan1024@naver.com>
2024-05-03 15:55:13 +0900

    feature-cors : 주석 추가

--- src/main/java/ggs/srr/config/WebConfig.java
+++ src/main/java/ggs/srr/config/WebConfig.java
@@ -7,10 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
+    /**
+     * CORS Config
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        // 모든 경로에 접속 허용
         registry.addMapping("/**")
+                // 모든 도메인 접속 허용
                 .allowedOrigins("*")
+                // GET, POST 방식 요청 허용
                 .allowedMethods("GET", "POST");
     }
 }
