c6925a1b38370080ac3d6d6e30094d847878f408
조재영 <whwodud0303@naver.com>
2024-10-08 22:45:25 +0900

    feat: Endpoint EvalRanking

--- src/main/java/ggs/srr/controller/user/dto/RankingEvalPointDto.java
+++ src/main/java/ggs/srr/controller/user/dto/RankingEvalPointDto.java
@@ -1,2 +1,16 @@
-package ggs.srr.controller.user.dto;public class RankingEvalPointDto {
+package ggs.srr.controller.user.dto;
+
+import lombok.Data;
+
+@Data
+public class RankingEvalPointDto {
+    private String photo;
+    private String intraId;
+    private int evalPoint;
+
+    public RankingEvalPointDto(String photo, String intraId, int evalPoint) {
+        this.photo = photo;
+        this.intraId = intraId;
+        this.evalPoint = evalPoint;
+    }
 }
--- src/main/java/ggs/srr/service/ranking/RankingService.java
+++ src/main/java/ggs/srr/service/ranking/RankingService.java
@@ -1,2 +1,39 @@
-package ggs.srr.service.ranking;public class RankingService {
+package ggs.srr.service.ranking;
+
+import ggs.srr.controller.user.dto.RankingEvalPointDto;
+import ggs.srr.domain.user.FtUser;
+import ggs.srr.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RankingService {
+
+    private final UserRepository userRepository;
+
+    public List<RankingEvalPointDto> rankingEvalPoint() {
+        List<FtUser> ftUsers = userRepository.findAll();
+        List<RankingEvalPointDto> result = new ArrayList<>();
+
+        Collections.sort(ftUsers, new Comparator<FtUser>() {
+            @Override
+            public int compare(FtUser o1, FtUser o2) {
+                return o2.getCollectionPoint() - o1.getCollectionPoint();
+            }
+        });
+
+        ftUsers.forEach(ftUser -> {
+            if (ftUser.getLevel() != 0.00)
+                result.add(new RankingEvalPointDto(ftUser.getImage(), ftUser.getIntraId(), ftUser.getCollectionPoint()));
+        });
+        return result;
+    }
 }
