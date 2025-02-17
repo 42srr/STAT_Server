97da18d6218cf800daf426d32b475067da894f6f
joonwan <joonwan1024@naver.com>
2025-02-18 01:25:17 +0900

    test: 스터디룸 repository 계층 테스트 코드 작성

---
+++ src/test/java/ggs/srr/repository/studyroom/StudyRoomRepositoryTest.java
@@ -0,0 +1,36 @@
+package ggs.srr.repository.studyroom;
+
+import ggs.srr.domain.studyroom.StudyRoom;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+class StudyRoomRepositoryTest {
+
+    @Autowired StudyRoomRepository repository;
+
+    @DisplayName("스터디룸을 저장 및 조회할 수 있어야 한다.")
+    @Test
+    void save() {
+        //given
+        StudyRoom studyRoom = new StudyRoom("test", "image", null, null, 0,0,0,0);
+        repository.save(studyRoom);
+
+        //when
+        StudyRoom findStudyRoom = repository.findById(studyRoom.getId());
+
+        //then
+        assertThat(findStudyRoom).isNotNull();
+        assertThat(findStudyRoom.getName()).isEqualTo("test");
+    }
+
+}
