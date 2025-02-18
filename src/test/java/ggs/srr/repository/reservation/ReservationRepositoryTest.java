d8f3f3e5a65ea9deade94e539d158bdec62b87a8
joonwan <joonwan1024@naver.com>
2025-02-18 18:25:31 +0900

    fix: Reservation Respository 에 맞게 테스트 코드 수정

--- src/test/java/ggs/srr/repository/reservation/ReservationRepositoryTest.java
+++ src/test/java/ggs/srr/repository/reservation/ReservationRepositoryTest.java
@@ -7,6 +7,7 @@ import ggs.srr.domain.studyroom.StudyRoom;
 import ggs.srr.repository.studygroup.exception.FindIdNullException;
 import ggs.srr.repository.studygroup.studygroup.StudyGroupRepository;
 import ggs.srr.repository.studyroom.StudyRoomRepository;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,6 +34,8 @@ public class ReservationRepositoryTest {
     private StudyRoomRepository studyRoomRepository;
     @Autowired
     private StudyGroupRepository studyGroupRepository;
+    @Autowired
+    private EntityManager em;
 
     @DisplayName("예약을 저장할 경우 스터디룸 id로 조회가능 해야 한다.")
     @Test
@@ -83,34 +86,70 @@ public class ReservationRepositoryTest {
     void findByTimeForUpdate() {
         //given
         LocalDate date = LocalDate.of(2025, 2, 10);
-        List<Reservation> reservations = createReservations(date);
+
+        List<StudyRoom> studyRooms = createStudyRoom();
+        studyRooms.forEach(studyRoomRepository::save);
+
+        List<Reservation> reservations = createReservations(date, studyRooms.get(0), studyRooms.get(1));
         reservations.forEach(reservationRepository::save);
 
-        LocalDateTime startDateTime = LocalDateTime.of(date, LocalTime.of(11, 0));
-        LocalDateTime endDateTime = LocalDateTime.of(date, LocalTime.of(15, 0));
+        em.flush();
+        em.clear();
+
+        LocalDateTime startDateTime = LocalDateTime.of(date, LocalTime.of(12, 0));
+        LocalDateTime ebdDateTime = LocalDateTime.of(date, LocalTime.of(18, 0));
+        StudyRoom wantedStudyRoom1 = studyRooms.get(0);
+        StudyRoom wantedStudyRoom2 = studyRooms.get(1);
 
         //when
-        List<Reservation> findReservations = reservationRepository.findByTimeForUpdate(startDateTime, endDateTime);
+        List<Reservation> findReservations1 = reservationRepository.findByTimeForUpdate(startDateTime, ebdDateTime, wantedStudyRoom1);
+        List<Reservation> findReservations2 = reservationRepository.findByTimeForUpdate(startDateTime, ebdDateTime, wantedStudyRoom2);
+
 
         //then
-        assertThat(findReservations.size()).isEqualTo(3);
-
-
+        assertThat(findReservations1.size()).isEqualTo(3);
+        assertThat(findReservations2.size()).isEqualTo(2);
     }
 
-    private List<Reservation> createReservations(LocalDate date) {
+    /*
+        todo
+        쭝구리가 내일 해야할 일
+        1. 기존 예약 조회시 스터디룸 아이디 또한 파라미터로 넘기게 변경해야 한다잇
+        2. 그에 맞는 테스트 코드도 짜야 한다잇
+        3. 동시성 테스트 까지 완료 해야 한다잇
+        4. 나머지는 노가다로 클리어 해야 한다잇
+
+    */
+
+    private List<StudyRoom> createStudyRoom() {
+        StudyRoom s1 = new StudyRoom("s1", null, LocalTime.of(9, 0), LocalTime.of(18, 0), 0, 0,0,0);
+        StudyRoom s2 = new StudyRoom("s2", null, LocalTime.of(9, 0), LocalTime.of(18, 0), 0, 0,0,0);
+        return List.of(s1, s2);
+    }
+
+    private List<Reservation> createReservations(LocalDate date, StudyRoom s1, StudyRoom s2) {
         Reservation r1 = new Reservation();
         Reservation r2 = new Reservation();
         Reservation r3 = new Reservation();
         Reservation r4 = new Reservation();
-
+        Reservation r5 = new Reservation();
+        Reservation r6 = new Reservation();
 
         r1.initializeReservationDateTime(LocalDateTime.of(date, LocalTime.of(10, 0)), LocalDateTime.of(date, LocalTime.of(12, 0)));
         r2.initializeReservationDateTime(LocalDateTime.of(date, LocalTime.of(12, 0)), LocalDateTime.of(date, LocalTime.of(14, 0)));
         r3.initializeReservationDateTime(LocalDateTime.of(date, LocalTime.of(14, 0)), LocalDateTime.of(date, LocalTime.of(16, 0)));
         r4.initializeReservationDateTime(LocalDateTime.of(date, LocalTime.of(16, 0)), LocalDateTime.of(date, LocalTime.of(18, 0)));
+        r5.initializeReservationDateTime(LocalDateTime.of(date, LocalTime.of(12, 0)), LocalDateTime.of(date, LocalTime.of(18, 0)));
+        r6.initializeReservationDateTime(LocalDateTime.of(date, LocalTime.of(14, 0)), LocalDateTime.of(date, LocalTime.of(18, 0)));
 
-        return List.of(r1, r2, r3, r4);
+        r1.initializeStudyRoom(s1);
+        r2.initializeStudyRoom(s1);
+        r3.initializeStudyRoom(s1);
+        r4.initializeStudyRoom(s1);
+        r5.initializeStudyRoom(s2);
+        r6.initializeStudyRoom(s2);
+
+        return List.of(r1, r2, r3, r4, r5, r6);
 
     }
 }
