1b39b09657dd98cccacdd1535278532cbc82af23
joonwan <joonwan1024@naver.com>
2025-02-23 15:43:18 +0900

    feat: study room repo 단건 조회시 비관락 적용 메서드 구현

--- src/main/java/ggs/srr/repository/studyroom/StudyRoomRepository.java
+++ src/main/java/ggs/srr/repository/studyroom/StudyRoomRepository.java
@@ -2,10 +2,9 @@ package ggs.srr.repository.studyroom;
 
 import ggs.srr.domain.studyroom.StudyRoom;
 import ggs.srr.repository.studygroup.exception.FindIdNullException;
-import ggs.srr.service.studyroom.request.CreateStudyRoomServiceRequest;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
 import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,13 +13,23 @@ public class StudyRoomRepository {
     @PersistenceContext
     private EntityManager em;
 
-    @Transactional
-    public void save(StudyRoom studyRoom) { em.persist(studyRoom); }
+    public void save(StudyRoom studyRoom) {
+        em.persist(studyRoom);
+    }
 
     public StudyRoom findById(Long id) {
         if (id == null) {
             throw new FindIdNullException("id로 studyroom 조회시 null을 전달할 수 없습니다.");
         }
+
         return em.find(StudyRoom.class, id);
     }
+
+    public StudyRoom findByIdForUpdate(Long id) {
+        if (id == null) {
+            throw new FindIdNullException("id로 studyroom 조회시 null을 전달할 수 없습니다.");
+        }
+        return em.find(StudyRoom.class, id, LockModeType.PESSIMISTIC_READ);
+    }
+
 }
