910e914ae2d57d499d24ef52d229e847394c37a8
joonwan <joonwan1024@naver.com>
2025-02-23 15:40:45 +0900

    feat: Reservation 시간, 스터디룸 아이디 조회시 비관락 적용 메서드 구현

--- src/main/java/ggs/srr/repository/reservation/ReservationRepository.java
+++ src/main/java/ggs/srr/repository/reservation/ReservationRepository.java
@@ -5,16 +5,20 @@ import ggs.srr.domain.studygroup.StudyGroup;
 import ggs.srr.domain.studyroom.StudyRoom;
 import ggs.srr.repository.reservation.exception.FindByNullException;
 import ggs.srr.repository.studygroup.exception.FindIdNullException;
+import ggs.srr.service.reservation.exception.NotFoundReservationException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.PersistenceContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Repository
 public class ReservationRepository {
+
     @PersistenceContext
     private EntityManager em;
 
@@ -22,12 +26,6 @@ public class ReservationRepository {
         em.persist(reservation);
     }
 
-    //jpql
-    /*
-    @todo
-     스터디룸 아이디 별 전체 예약 조회
-     스터디 그롭 아이디 별 전체 예약 조회
-     */
     public List<Reservation> findByStudyRoomId(Long studyRoomId) {
         if (studyRoomId == null) {
             throw new FindIdNullException("예약 id로 예약 조회시 null을 전달할 없습니다.");
@@ -55,8 +53,7 @@ public class ReservationRepository {
         return em.find(Reservation.class, reservationId);
     }
 
-    public void delete(Long studyRoomId) {
-        Reservation reservation = em.find(Reservation.class, studyRoomId);
+    public void remove(Reservation reservation) {
         if (reservation != null) {
             em.remove(reservation);
         }
@@ -67,11 +64,12 @@ public class ReservationRepository {
         if (startDateTime == null || endDateTime == null || studyRoom == null) {
             throw new FindByNullException("null 을 이용해 해당 시간대의 예약 목록을 조회할 수 없습니다.");
         }
-
         return em.createQuery("select r from Reservation r where r.studyRoom = :studyRoom and r.startDateTime < :endDateTime and r.endDateTime > :startDateTime", Reservation.class)
                 .setParameter("studyRoom", studyRoom)
                 .setParameter("startDateTime", startDateTime)
                 .setParameter("endDateTime", endDateTime)
+                .setLockMode(LockModeType.PESSIMISTIC_WRITE)
                 .getResultList();
     }
+
 }
