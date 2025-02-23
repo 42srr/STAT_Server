96c3962ad265483ab3814aac2f76c125a9781317
joonwan <joonwan1024@naver.com>
2025-02-23 15:42:11 +0900

    feat: study group 단건 조회시 비관락 적용 메서드 구현

--- src/main/java/ggs/srr/repository/studygroup/studygroup/StudyGroupRepository.java
+++ src/main/java/ggs/srr/repository/studygroup/studygroup/StudyGroupRepository.java
@@ -3,8 +3,12 @@ package ggs.srr.repository.studygroup.studygroup;
 import ggs.srr.domain.studygroup.StudyGroup;
 import ggs.srr.repository.studygroup.exception.FindIdNullException;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudyGroupRepository {
@@ -22,6 +26,15 @@ public class StudyGroupRepository {
         }
 
         return em.find(StudyGroup.class, groupId);
+    }
+
+
+    public StudyGroup findByIdForUpdate(Long groupId) {
+        if (groupId == null) {
+            throw new FindIdNullException("id 로 그룹 조회시 null 을 전달할 수 없습니다.");
+        }
+
+        return em.find(StudyGroup.class, groupId, LockModeType.PESSIMISTIC_READ);
     }
 
 }
