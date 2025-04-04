15034974629d40a02f5c6696f20b026d3c3bae37
joonwan <joonwan1024@naver.com>
2025-04-05 01:29:20 +0900

    feat: 사용자 조회 기능 구현

--- src/main/java/ggs/srr/service/user/UserService.java
+++ src/main/java/ggs/srr/service/user/UserService.java
@@ -1,105 +1,40 @@
 package ggs.srr.service.user;
 
-import ggs.srr.api.controller.user.dto.UserResponse;
-import ggs.srr.domain.user.FtUser;
+import ggs.srr.domain.user.User;
+import ggs.srr.exception.service.user.NotFoundUserException;
 import ggs.srr.repository.user.UserRepository;
-import java.util.ArrayList;
-import java.util.Comparator;
+import ggs.srr.service.user.request.UserInformationServiceRequest;
+import ggs.srr.service.user.response.UserInformationResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
     }
 
-    @Transactional(readOnly = false)
-    public long save(FtUser user) {
-        return userRepository.save(user);
+    public UserInformationResponse findById(UserInformationServiceRequest request, LocalDateTime now) {
+        User findUser = userRepository.findById(request.getId())
+                .orElseThrow(() -> new NotFoundUserException("해당 사용자를 찾을 수 없습니다."));
+
+        boolean updatable = isUpdatableUser(findUser, now);
+        return new UserInformationResponse(findUser, updatable);
     }
 
-    @Transactional(readOnly = false)
-    public void updateJWTTokens(String intraId, String accessToken, String refreshToken) {
-        Optional<FtUser> byIntraId = findByIntraId(intraId);
-        if (byIntraId.isEmpty())
-            throw new RuntimeException("intra id is invalid");
-        FtUser findUser = byIntraId.get();
-        findUser.updateRefreshToken(refreshToken);
+    private boolean isUpdatableUser(User user, LocalDateTime now) {
+        return Duration.between(user.getUpdatedAt(), now).toHours() >= 12;
     }
 
-    public FtUser findById(long id) {
-        return userRepository.findById(id);
-    }
-
-    public Optional<FtUser> findByIntraId(String intraId){
-        return userRepository.findByIntraId(intraId);
-    }
-
-    public List<UserResponse> findAll(){
-        List<UserResponse> users = new ArrayList<>();
-        List<FtUser> findUsers = userRepository.findAll();
-
-        findUsers.forEach((ftUser) -> users.add(new UserResponse(ftUser)));
-        return users;
-    }
-
-    public UserResponse findByIntraIdForApi(String intraId) {
-        Optional<FtUser> byIntraId = userRepository.findByIntraId(intraId);
-        if (byIntraId.isEmpty()) {
-            throw new IllegalArgumentException("intra id 에 해당하는 사용자가 존재하지 않습니다.");
-        }
-        FtUser user = byIntraId.get();
-        return new UserResponse(user);
-    }
-
-    public LevelResponseList getLevelInfo() {
-        List<FtUser> users = userRepository.findAll();
-        List<LevelResponse> levelResponseList = new ArrayList<>();
-        initializeLevel(levelResponseList);
-
-        for (FtUser user : users) {
-            int level = (int) Math.floor(user.getLevel());
-            updateLevel(levelResponseList, level);
-        }
-
-        return new LevelResponseList(levelResponseList);
-    }
-
-    private void initializeLevel(List<LevelResponse> levelResponseList) {
-        for (int i = 0; i < 22; i++) {
-            levelResponseList.add(new LevelResponse(i, 0));
-        }
-    }
-
-    private void updateLevel(List<LevelResponse> levelResponseList, int level) {
-        if (!isExistingLevel(levelResponseList.size(), level)) {
-            addExtraLevel(levelResponseList, level);
-        }
-        if (isExistingLevel(levelResponseList.size(), level)) {
-            increaseLevelCount(levelResponseList, level);
-        }
-    }
-
-    private boolean isExistingLevel(int levelSize, int level) {
-        return level >= 0 && level < levelSize;
-    }
-
-    private void addExtraLevel(List<LevelResponse> levelResponseList, int level) {
-        for (int i = 0; i <= level - levelResponseList.size() + 1; i++) {
-            levelResponseList.add(new LevelResponse(levelResponseList.size() + 1, 0));
-        }
-    }
-    
-    private void increaseLevelCount(List<LevelResponse> levelResponseList, int level) {
-        LevelResponse existingLevel = levelResponseList.get(level);
-        levelResponseList.set(level, new LevelResponse(existingLevel.getLevel(), existingLevel.getCount() + 1));
-    }
 }
