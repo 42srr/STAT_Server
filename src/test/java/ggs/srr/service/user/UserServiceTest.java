c5578f410c97a69221d2bc31e34952c702a934ec
joonwan <joonwan1024@naver.com>
2025-04-05 02:07:30 +0900

    test: 사용자 레벨 분포 조회 기능 테스트 코드 작성

--- src/test/java/ggs/srr/service/user/UserServiceTest.java
+++ src/test/java/ggs/srr/service/user/UserServiceTest.java
@@ -3,6 +3,7 @@ package ggs.srr.service.user;
 import ggs.srr.domain.user.User;
 import ggs.srr.repository.user.UserRepository;
 import ggs.srr.service.user.request.UserInformationServiceRequest;
+import ggs.srr.service.user.response.LevelDistributionResponse;
 import ggs.srr.service.user.response.UserInformationResponse;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
@@ -69,5 +70,40 @@ class UserServiceTest {
 
         //then
         assertThat(response.isUpdatable()).isTrue();
+    }
+
+    @DisplayName("레벨별 사용자 분포를 확인할 수 있어야 한다.")
+    @Test
+    void getUserLevelDistribution() {
+        //given
+        User user1 = createUserBy("test1", 1.2);
+        User user2 = createUserBy("test2", 1.7);
+        User user3 = createUserBy("test3", 2.1);
+        User user4 = createUserBy("test4", 4.2);
+        User user5 = createUserBy("test5", 5.2);
+        User user6 = createUserBy("test6", 5.9);
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+        userRepository.save(user4);
+        userRepository.save(user5);
+        userRepository.save(user6);
+
+        //when
+        LevelDistributionResponse userLevelDistribution = userService.getUserLevelDistribution();
+
+        //then
+        assertThat(userLevelDistribution.getDistribution().get(1)).isEqualTo(2);
+        assertThat(userLevelDistribution.getDistribution().get(2)).isEqualTo(1);
+        assertThat(userLevelDistribution.getDistribution().get(4)).isEqualTo(1);
+        assertThat(userLevelDistribution.getDistribution().get(5)).isEqualTo(2);
+    }
+
+    private User createUserBy(String intraId, double level) {
+        return User.builder()
+                .intraId(intraId)
+                .level(level)
+                .build();
     }
 }
