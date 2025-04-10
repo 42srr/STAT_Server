1b27de4e36c1da50c048e8472a135b0c5f34a8df
joonwan <joonwan1024@naver.com>
2025-04-10 23:02:27 +0900

    test: Project controller 테스트 코드 작성

--- src/main/java/ggs/srr/repository/studygroup/exception/FindIdNullException.java
+++
@@ -1,8 +0,0 @@
-package ggs.srr.repository.studygroup.exception;
-
-public class FindIdNullException extends RuntimeException {
-
-    public FindIdNullException(String message) {
-        super(message);
-    }
-}
