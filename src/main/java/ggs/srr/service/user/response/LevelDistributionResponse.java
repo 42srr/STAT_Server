2591faea36852b57b61bcc5b396fc95855e18988
joonwan <111417356+joonwan@users.noreply.github.com>
2025-04-13 15:56:42 +0900

    feat: swagger 설정 (#66)

--- src/main/java/ggs/srr/service/user/response/LevelDistributionResponse.java
+++ src/main/java/ggs/srr/service/user/response/LevelDistributionResponse.java
@@ -1,12 +1,15 @@
 package ggs.srr.service.user.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.Map;
 
 @Getter
+@Schema(description = "사용자 레벨 분포")
 public class LevelDistributionResponse {
 
+    @Schema(description = "key : 레벨, value : 해당 레벨 인원 수")
     private Map<Integer, Long> distribution;
 
     public LevelDistributionResponse(Map<Integer, Long> distribution) {
