bc8a10e6f7acf897550791ace83658150e3dbdb1
joonwan <joonwan1024@naver.com>
2025-04-18 02:28:25 +0900

    test: 한번더 잘못된 테스트 코드

--- src/test/java/ggs/srr/api/controller/project/ProjectControllerTest.java
+++ src/test/java/ggs/srr/api/controller/project/ProjectControllerTest.java
@@ -58,7 +58,7 @@ class ProjectControllerTest {
         mockMvc.perform(
                         get("/api/projects/distribution?type=finished")
                 ).andDo(print())
-                .andExpect(status().isBadRequest())
+                .andExpect(status().isBadRequest()) // 한번더
                 .andExpect(jsonPath("$.code").value("200"))
                 .andExpect(jsonPath("$.status").value("OK"))
                 .andExpect(jsonPath("$.data").exists());
