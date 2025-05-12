abe65c8864d8e077b96e5729a9ea2f525a344dc3
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2025-05-12 13:44:07 +0900

    feat : error코드 분리

--- src/main/java/ggs/srr/exception/project/ProjectException.java
+++ src/main/java/ggs/srr/exception/project/ProjectException.java
@@ -1,7 +1,12 @@
 package ggs.srr.exception.project;
 
+import org.springframework.http.HttpStatus;
+
 public class ProjectException extends RuntimeException {
-  public ProjectException(String message) {
-    super(message);
+  private HttpStatus httpStatus;
+
+  public ProjectException(ProjectErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.httpStatus = errorCode.getHttpStatus();
   }
 }
--- src/main/java/ggs/srr/exception/user/UserErrorCode.java
+++ src/main/java/ggs/srr/exception/user/UserErrorCode.java
@@ -6,7 +6,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserErrorCode {
 
-    NOT_FOUND_USER("해당 사용자를 조회할 수 없습니다.", HttpStatus.BAD_REQUEST);
+    NOT_FOUND_USER("해당 사용자를 조회할 수 없습니다.", HttpStatus.BAD_REQUEST),
+    UPDATE_NOT_AVAILABLE("마지막 업데이트 후 12시간이 지나야 다시 업데이트할 수 있습니다.", HttpStatus.BAD_REQUEST),
+    API_FETCH_FAILED("API 접근에 실패했습니다.", HttpStatus.BAD_REQUEST),
+    OAUTH_TOKEN_NOT_FOUND("인증 토큰이 없습니다.", HttpStatus.BAD_REQUEST),
+    ACCESS_TOKEN_EXPIRED("인증 토큰이 만료되었습니다. 다시 로그인해주세요.", HttpStatus.BAD_REQUEST),
+    ;// 확인해봐야함
 
     private String message;
     private HttpStatus httpStatus;
