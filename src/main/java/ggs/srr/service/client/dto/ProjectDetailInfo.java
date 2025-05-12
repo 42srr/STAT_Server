2d50d93bf77064dcc28967f4fe0747ae3def81bf
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2025-05-12 13:39:27 +0900

    refactor : class 명 수정

--- src/main/java/ggs/srr/service/client/dto/ProjectDetailInfo.java
+++ src/main/java/ggs/srr/service/client/dto/ProjectDetailInfo.java
@@ -1,10 +1,17 @@
 package ggs.srr.service.client.dto;
 
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class ProjectDetailInfo {
-    private String projectName;
-    private Integer projectFinalMark;
-    private String projectStatus;
+    private final String projectName;
+    private final Integer projectFinalMark;
+    private final String projectStatus;
+
+    public ProjectDetailInfo(String projectName, Integer projectFinalMark, String projectStatus) {
+        this.projectName = projectName;
+        this.projectFinalMark = projectFinalMark;
+        this.projectStatus = projectStatus;
+    }
 }
